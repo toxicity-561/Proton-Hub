8016d2c5356f783aa0d6828a391b125e201c1f42
T0xÏc <vaeros694@gmail.com>
2026-06-18 11:45:19 -0600

    Add loader function for external script

---
+++ loader.lua
@@ -0,0 +1,6 @@
+local function load(url)
+  loadstring(game:HttpGet(url))()
+end
+if game.PlaceId == 3623096087 then
+  load("https://raw.githubusercontent.com/toxicity-561/Games/refs/heads/main/MuscleLegends.luau")
+end
