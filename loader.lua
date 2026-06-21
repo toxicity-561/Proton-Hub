f90d3c442611b8d9d56a420461a534b35c4c3bef
T0xÏc <vaeros694@gmail.com>
2026-06-20 23:57:30 -0600

    Refactor game loading logic with a games table

--- loader.lua
+++ loader.lua
@@ -1,6 +1,12 @@
+local GAMES = {
+  ["MUSCLE LEGENDS"] = 3623096087,
+  ["GROW A GARDEN 2"] = 97598239454123
+};
 local function load(url)
-  loadstring(game:HttpGet(url))()
-end
-if game.PlaceId == 3623096087 then
-  load("https://raw.githubusercontent.com/toxicity-561/Games/refs/heads/main/MuscleLegends.luau")
-end
+  loadstring(game:HttpGet(url))();
+end;
+if game.PlaceId == GAMES["MUSCLE LEGENDS"] then
+  load("https://raw.githubusercontent.com/toxicity-561/Games/refs/heads/main/MuscleLegends.luau");
+elseif game.PlaceId == GAMES["GROW A GARDEN 2"] then
+  load("https://raw.githubusercontent.com/toxicity-561/Games/refs/heads/main/GrowAGarden2.luau");
+end;
